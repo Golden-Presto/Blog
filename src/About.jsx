c72aa00971c037a20e92c564373d04b2cf8da84a
Homagni <2333622@cognizant.com>
2024-06-08 03:49:14 +0530

    First commit

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+
+
+const About = () => {
+  return (
+    <main className="About">
+      <h2>About</h2>
+      <p style={{ marginTop: "1rem" }}>This blog app is a project taught by Dave Gray</p>
+    </main>
+  )
+}
+
+export default About
